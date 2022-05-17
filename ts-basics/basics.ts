edff2508eefab2a001da53fa873a229535f84770
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-05-17 09:13:02 +0200

    created new react with typescript project

--- ts-basics/basics.ts
+++ ts-basics/basics.ts
@@ -46,11 +46,14 @@ function printOutput(value: any) {
 
 // generics
 
-function insertAtBeginning(array: any[], value: any) {
+function insertAtBeginning<T>(array: T[], value: T) {
   const newArray = [value, ...array];
   return newArray;
 }
 
 const demoArray = [1, 2, 3];
 
-const updatedArray = insertAtBeginningd(demoArray, -1);
+const updatedArray = insertAtBeginning(demoArray, -1);
+const stringArray = insertAtBeginning(['a', 'b', 'c'], 'd');
+
+// updatedArray[0].split('')
