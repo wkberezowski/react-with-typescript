ba753b8a1b7fa9b9c0a39f60bacc0314d53d5633
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-05-17 11:48:26 +0200

    added refs, funtion props and state with typescript

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import './App.css';
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
-  const todos = [new Todo('learn react'), new Todo('learn typescript')];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHanlder = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
   return (
     <div className='App'>
+      <NewTodo onAddTodo={addTodoHanlder} />
       <Todos items={todos} />
     </div>
   );
